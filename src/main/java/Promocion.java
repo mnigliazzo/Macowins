4cd56730ee25db095e6881390f5558adb9f53fd5
Matias Nigliazzo <mnigliazzo@frba.utn.edu.ar>
2021-04-15 21:27:59 -0300

    Agrego Constructor

--- src/main/java/Promocion.java
+++ src/main/java/Promocion.java
@@ -1,7 +1,6 @@
-public class Promocion implements Estado
-{
+public class Promocion implements Estado{
   public int montoDescuento;
-
+  public Promocion(int _montoDescuento){montoDescuento=_montoDescuento;}
   public int getPrecioPorEstado(int _PrecioLista) {
     return _PrecioLista-montoDescuento;
   }
